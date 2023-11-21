ad69a0e96cb5921427fd7e5a41f40921218f1dfd
Pavlo <pavlokutsyn@gmail.com>
2023-11-21 22:07:43 +0100

    20231121/3

---
+++ SumCalculator/src/sumCalculator.java
@@ -0,0 +1,11 @@
+public class sumCalculator {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+        int sum = getSum (x, y);
+        System.out.println("Summ x + y = " +sum);
+    }
+    public static int getSum (int x, int y) {
+        return x + y;
+    }
+}
